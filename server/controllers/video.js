bfb0f9378092f4d108ac6da4e77d0256ddd2ed5b
Taras Labiak <kissarat@gmail.com>
2016-02-12 02:10:46 +0200

    + capabilities, fix upload folder

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var path = require('path');
+var _static = path.normalize(__dirname + '/../../static');
 
 module.exports = {
     index: function(_) {
@@ -30,7 +32,7 @@ module.exports = {
 
     upload: function(_) {
         var id = parseFloat(_.req.url.route[1]);
-        var filename = 'upload/' + id;
+        var filename = _static + '/upload/video/' + id;
         fs.open(filename, 'w', _.wrap(function(fd) {
             _.req.on('data', function(data) {
                 fs.write(fd, data, 0, data.byteLength, _.wrap(function(written, string) {
@@ -55,5 +57,42 @@ module.exports = {
                 }));
             });
         }));
+    },
+
+    capabilities: function(_) {
+        var ffmpeg = require('fluent-ffmpeg');
+
+        var list = ['formats', 'codecs', 'encoders', 'filters'];
+        list.reverse();
+        var result = {};
+        function collect() {
+            if (list.length <= 0) {
+                _.res.send(result);
+                return;
+            }
+            var capability = list.pop();
+            var method = 'getAvailable' + capability[0].toUpperCase() + capability.slice(1);
+            //console.log(method);
+            method = ffmpeg[method];
+            if (method) {
+                method(function (err, capabilities_list) {
+                    var info;
+                    if (err) {
+                        info = {error: err};
+                    }
+                    else {
+                        info = capabilities_list;
+                    }
+                    result[capability] = info;
+                    collect();
+                })
+            }
+            else {
+                console.error(capability + ' not found');
+                collect();
+            }
+        }
+
+        collect();
     }
 };
--- server/process.js
+++ server/process.js
@@ -3,45 +3,88 @@
 var MongoClient = require('mongodb').MongoClient;
 var ffmpeg = require('fluent-ffmpeg');
 var fs = require('fs');
-var db;
+var path = require('path');
+var _static = path.normalize(__dirname + '/../static');
 
-MongoClient.connect('mongodb://localhost:27017/socex', function (err, _db, done) {
-    db = _db;
-    db.collection('media').findOne({status: 'uploaded', type: 'video'}, function (err, result) {
+function query(collection, call) {
+    MongoClient.connect('mongodb://localhost:27017/socex', function (err, _db, done) {
         if (err) {
-            return console.log(err);
+            console.error(err);
         }
+        else if (_db) {
+            call(_db.collection(collection));
+        }
+    });
+}
+
+query('media', function (media) {
+    media.findOne({status: 'uploaded', type: 'video'}, function (err, result) {
         if (!result) {
-            return db.close();
+            process.exit();
         }
-        console.log(new Date() + ': ' + result._id + '. ' + result.title);
         var id = result._id;
-        var filename = 'upload/' + id;
-        ffmpeg(filename)
-            .audioBitrate('96k')
-            .audioFrequency(22050)
-            .audioFilters({
-                filter: 'silencedetect',
-                options: {n: '-50dB', d: 5}
-            })
-            .videoCodec('libx264')
-            .videoBitrate(680)
-            .fps(29.7)
-            .output('video/' + id + '.mp4')
-            .on('end', function () {
-                fs.unlink(filename, function () {
-                    db.collection('media')
-                        .update({_id: id}, {$set: {converted: Date.now(), status: 'done'}}, function(err, r) {
-                            if (err) {
-                                console.log(err);
-                            }
-                            else {
-                                console.log('done ' + new Date() + ': ' + id + '. ' + result.title);
-                            }
-                            db.close();
-                        });
+        var filename = _static + '/upload/video/' + id;
+        if (fs.existsSync(filename)) {
+            console.log(new Date() + ': ' + result._id + '. ' + result.title);
+            convert(result, filename);
+        }
+        else {
+            console.error('not exists\t' + filename);
+            query('media', function (media) {
+                media.deleteOne({_id: id}, function (err, result) {
+                    console.log('deleted\t' + JSON.stringify(result));
+                    process.exit();
                 });
-            })
-            .run();
+            });
+        }
     })
 });
+
+function convert(info, filename) {
+    ffmpeg(filename)
+        .audioBitrate('96k')
+        .audioFrequency(22050)
+        .audioFilters({
+            filter: 'silencedetect',
+            options: {n: '-50dB', d: 5}
+        })
+        .videoCodec('libx264')
+        .videoBitrate(680)
+        //.fps(23.976)
+        .screenshots({
+            timestamps: ['50%'],
+            folder: _static + '/video/thumbnail',
+            filename: info._id + '.jpg',
+            size: '480x320'
+        })
+        .output(_static + '/video/' + info._id + '.mp4')
+        .on('codecData', function (data) {
+            console.log(data);
+            query('media', function (media) {
+                media.update({_id: info._id}, {$set: {input: data}});
+            })
+        })
+        .on('process', function (process) {
+            console.log(JSON.stringify(process))
+        })
+        .on('error', function (error) {
+            console.error(error)
+        })
+        .on('end', function () {
+            fs.unlink(filename, function () {
+                query('media', function (media) {
+                    media.update({_id: info._id}, {
+                        $set: {
+                            converted: Date.now(),
+                            status: 'done'
+                        }
+                    }, function (err, r) {
+                        console.log('done ' + new Date() + ': ' + id + '. ' + info.title);
+                        db.close();
+                    })
+                });
+            });
+        })
+        .run();
+}
+
