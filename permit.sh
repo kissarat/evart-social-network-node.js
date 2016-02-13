0442605f197d006c3e5dc0ca376fc76865c90012
Taras Labiak <kissarat@gmail.com>
2016-02-13 02:08:35 +0200

    + nginx.conf

--- permit.sh
+++ permit.sh
@@ -3,6 +3,8 @@ mkdir static
 mkdir static/photo
 mkdir -p static/video/thumbnail
 mkdir static/upload/video
+ln -s static/video app/video
+ln -s static/photo app/photo
 mkdir server/nginx/logs
 chmod 777 server/nginx/logs
 if [ -f /usr/local/bin/mongod ]
@@ -10,4 +12,5 @@ then
     mkdir server/mongodb/data
     chmod 777 server/mongodb/data
     /usr/local/bin/mongod --dbpath=server/mongodb/data &
+    ps -A | grep mongod
 fi
