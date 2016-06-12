7c92e2000fe322240a439290a3cb8acdf41544c1
Taras Labiak <kissarat@gmail.com>
2016-06-12 23:19:37 +0300

    + jquery core

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,76 +1,50 @@
-App.pull = function () {
-    if (this.socket && WebSocket.OPEN === this.socket.readyState) {
-        return;
-    }
-    this.socket = new WebSocket(App.config.socket.address);
-    register(this.socket, {
-        // open: function () {
-        //     console.log('Socket connected at ' + new Date().toLocaleString());
-        // },
-        message: function (e) {
-            App.debug.push('socket_pull', e.data);
-            try {
-                var message = JSON.parse(e.data);
-            } catch (ex) {
-                return console.error('INVALID_JSON', e.data);
-            }
-            if (message.type) {
-                return App.trigger(message.type, message);
-            } else {
-                return console.error('UNKNOWN_MESSAGE', message);
-            }
-        },
-        close: function () {
-            if (App.user) {
-                return setTimeout(pull, App.config.socket.wait);
-            }
-        }
-    });
-};
+"use strict";
 
-function pull() {
-    return App.pull();
+function Socket(options) {
+    this.address = options.address;
 }
 
-App.push = function (message) {
-    if ('string' !== typeof message) {
-        message.source_id = App.user._id;
-        message = JSON.stringify(message);
+Socket.prototype = {
+    pull: function () {
+        if (this.socket && WebSocket.OPEN === this.socket.readyState) {
+            return;
+        }
+        this.socket = new WebSocket(this.address);
+        register(this.socket, {
+            message: function (e) {
+                App.debug.push('socket_pull', e.data);
+                try {
+                    var message = JSON.parse(e.data);
+                } catch (ex) {
+                    return console.error('INVALID_JSON', e.data);
+                }
+                if (message.type) {
+                    return App.trigger(message.type, message);
+                } else {
+                    return console.error('UNKNOWN_MESSAGE', message);
+                }
+            },
+            close: function () {
+                if (App.user) {
+                    return setTimeout(pull, App.config.socket.wait);
+                }
+            }
+        });
+    },
+
+    push: function (message) {
+        if ('string' !== typeof message) {
+            message.source_id = App.user._id;
+            message = JSON.stringify(message);
+        }
+        App.debug.push('socket_push', message);
+        return this.socket.send(message);
     }
-    App.debug.push('socket_push', message);
-    return this.socket.send(message);
 };
 
-register(App, {
-    login: pull,
-    logout: function () {
-        return App.socket.close();
-    },
-    message: function (message) {
-        var add, dialog;
-        dialog = App.getDialogs().findWhere({
-            dialog_id: message.source
-        });
-        add = function () {
-            return dialog.get('messages').add(message);
-        };
-        return App.dock.set('dialogs');
-    }
-});
-
- function notification_not_available() {
+var notification_not_available = function() {
     App.features.notification.enabled = false;
     return console.warn('Notification not available');
-}
-
-App.notify = function (title, options) {
-    options.title = title;
-    return {
-        options: options,
-        addEventListener: function (event, cb) {
-            return console.warn("LISTENER REGISTER");
-        }
-    };
 };
 
 if (App.features.notification.available && App.features.notification.enabled) {
@@ -86,3 +60,36 @@ if (App.features.notification.available && App.features.notification.enabled) {
 } else {
     notification_not_available();
 }
+
+App.notify = function (title, options) {
+    options.title = title;
+    return {
+        options: options,
+        addEventListener: function (event, cb) {
+            return console.warn("LISTENER REGISTER");
+        }
+    };
+};
+
+App.socket = new Socket(App.config.socket);
+
+// App.pull = App.socket.pull;
+// App.push = App.socket.push;
+
+
+register(App, {
+    login: App.socket.pull.bind(App.socket),
+    logout: function () {
+        return App.socket.close();
+    },
+    message: function (message) {
+        var add, dialog;
+        dialog = App.getDialogs().findWhere({
+            dialog_id: message.source
+        });
+        add = function () {
+            return dialog.get('messages').add(message);
+        };
+        return App.dock.set('dialogs');
+    }
+});
--- client/service.js
+++ client/service.js
@@ -1,9 +1,15 @@
 "use strict";
 
+importScripts(
+    '/js/jquery.core.js',
+    '/lib/underscore/underscore.js',
+    '/lib/backbone/backbone.js'
+);
+
 // var SOCEX_CACHE_NAME = 'socex';
 
-['install', 'activate', 'fetch', 'message', 'notificationclick', 'notificationclose', 'push', 'sync'].forEach(function (name) {
-    self.addEventListener(name, function (e) {
+// ['install', 'activate', 'fetch', 'message', 'notificationclick', 'notificationclose', 'push', 'sync'].forEach(function (name) {
+//     self.addEventListener(name, function (e) {
         // console.log(e);
-    });
-});
+    // });
+// });
